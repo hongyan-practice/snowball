8ee262674a1526dcc30f1508d9b12226b2b6aa3e
shinny-hongyan <hongyan@shinnytech.com>
2022-05-04 14:19:54 +0800

    add wss

--- snowball/wind.py
+++ snowball/wind.py
@@ -36,3 +36,21 @@ class wind(object):
             return content
         else:
             raise Exception("获取日时间序列函数失败 (%d,%s)" % (response.status_code, json.loads(response.content)))
+
+    def wss(self, codes: Union[str, List[str]], fields: Union[str, List[str]], options: str):
+        """
+        获取日时间序列函数
+
+        参考：https://www.windquant.com/qntcloud/apiRefHelp/id-91573a98-70d5-4462-8c6f-546ab45c8652
+        :return:
+        """
+        payload = {}
+        payload["codes"] = codes
+        payload["fields"] = fields
+        payload["options"] = options
+        response = requests.post(url="http://" + self.front_url + ":8080/wss", headers=self.headers, data=payload, timeout=30)
+        if response.status_code == 200:
+            content = json.loads(response.content)
+            return content
+        else:
+            raise Exception("获取日时间序列函数失败 (%d,%s)" % (response.status_code, json.loads(response.content)))
